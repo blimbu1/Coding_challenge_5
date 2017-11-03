9f2dcbebbd9b31bb5a95e0207811093ef8f6ebd1
Limbu <limbub@ons.gov.uk>
2017-11-03 18:25:30 +0000

    No comment

--- Binay.py
+++ Binay.py
@@ -15,19 +15,19 @@ data = pd.read_csv(filename,header=None)#saves the files as a dataframe
 data.columns = ['Name','Role','cost','Salary']
 #data['Role'].str.strip()               #Tried this method. Doesn't seem to work
 #data['Role']=data['Role'].map(lambda x:x.strip(' ')) #this is for stripping the spaces
-print(data.columns.values)              #prints the name of the columns.
+#print(data.columns.values)              #prints the name of the columns.
 #data.set_index('Role',inplace=True)     #Setting the 'Role' Column as the index
 #print(data)                             
-print(data.columns.values)              #Prints the new column values
+#print(data.columns.values)              #Prints the new column values
 #print(data.index)                      #Prints the index
 #print(data['Salary'].loc['Marketing'].mean(axis=0)) #Calculates the mean.
 #print(g3)
 df1 = data.groupby(['Role']).agg(['mean','count','max'])
 df1.columns = ['avg','count','max_sal']
 df2 = df1.sort_values('avg',ascending=False)
-print(df2)
-print(df1.index.name)
-print(df1.columns.values)
+#print(df2)
+#print(df1.index.name)
+#print(df1.columns.values)
 for index,row in df2.iterrows():
     if row['count'] >=8 and row['max_sal']<=152000:
         print(index, row['avg'])
